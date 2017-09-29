24aab261a76be0398879b1584196f82b23123cb8
AbhijithK <abhijith.k2012@gmail.com>
2017-09-29 03:26:36 -0400

    base template design and static files

--- akshaya/settings.py
+++ akshaya/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'akshaya.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["akshaya/templates/akshaya"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,8 @@ DATE_INOUT_FORMATS = ('%d-%m-%Y')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'),]
 STATIC_URL = '/static/'
+
 
 LOGIN_REDIRECT_URL="/staffmanagement/date"
